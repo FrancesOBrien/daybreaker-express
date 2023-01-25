c71f2c93a40a6a907ca11c987848d6a7a96dc87a
Frances O'Brien <jfrancesobrien@gmail.com>
2023-01-25 13:33:19 -0500

    cors lite

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ db.on('disconnected', () => console.log('mongo disconnected'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static('public'))
-app.use(cors())
+app.use(cors({ origin: '*' }))
 
 //Routes
 const tasksController = require('./controllers/tasks')
